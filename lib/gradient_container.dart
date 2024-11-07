7fddca7b0ed371d25b4ba393f9523226c102bb75
KotenkoNata <kotenkonata8@gmail.com>
2024-11-07 16:50:54 +0100

    feat: Practice: Reusable Widgets & Constructor Functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,23 +5,22 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors:
-        [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ],
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [color1, color2],
             begin: startAlignment,
             end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText('Hello world eewr!'),
+        child: StyledText('Hello world!'),
       ),
     );
   }
