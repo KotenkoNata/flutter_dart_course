bda3804f8efc1c0bea203d139dca19ccbe2f2053
KotenkoNata <kotenkonata8@gmail.com>
2024-11-11 17:10:38 +0100

    feat: Challenge Solution 2/2 - Working with More Widgets

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,7 +4,29 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
-  Widget build(context){
-    return const Text('Some text');
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white
+            ),
+              onPressed: (){},
+              child: const Text('Start quiz'))
+        ],
+      ),
+    );
   }
 }
