dd747cc8d61d11844c03cd7c7cabffdea43340ee
KotenkoNata <kotenkonata8@gmail.com>
2024-11-14 16:37:12 +0100

    feat: Passing Data via Functions Across Widgets

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,8 +12,8 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz>{
+  final List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
-
 
   void switchScreen() {
     setState(() {
@@ -21,12 +21,16 @@ class _QuizState extends State<Quiz>{
     });
   }
 
+  void chooseAnswer(String answer){
+    selectedAnswers.add(answer);
+  }
+
   @override
   Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if(activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
     }
 
     return MaterialApp(
