bc4d2c5972a35e9d161a9ad5ae3c8677e1102e36
KotenkoNata <kotenkonata8@gmail.com>
2024-11-28 16:54:54 +0100

    feat: Displaying Category Items on a Screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dart_course/widgets/category_grid_item.dart';
+import 'package:flutter_dart_course/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -15,13 +17,10 @@ class CategoriesScreen extends StatelessWidget {
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white),),
-          Text('2', style: TextStyle(color: Colors.white),),
-          Text('3', style: TextStyle(color: Colors.white),),
-          Text('4', style: TextStyle(color: Colors.white),),
-          Text('5', style: TextStyle(color: Colors.white),),
-          Text('6', style: TextStyle(color: Colors.white),),
+        children: [
+          // availableCategories.map()
+          for(final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
