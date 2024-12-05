b160c3046924435ba10bc5b8154e8d79f6b87759
KotenkoNata <kotenkonata8@gmail.com>
2024-12-05 16:41:31 +0100

    feat: Explicit Animations: Adding an Animation Controller

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,15 +5,40 @@ import 'package:flutter_dart_course/data/dummy_data.dart';
 import 'package:flutter_dart_course/models/category.dart';
 import 'package:flutter_dart_course/models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals});
 
   final List<Meal>availableMeals;
-  
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState(){
+    super.initState();
+
+    _animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 300),
+        lowerBound: 0,
+        upperBound: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category){
-    final filteredMeals = availableMeals.where((meal)=>meal.categories.contains(category.id)).toList();
+    final filteredMeals = widget.availableMeals.where((meal)=>meal.categories.contains(category.id)).toList();
 
     Navigator.push(context, MaterialPageRoute(builder: (ctx)=> MealsScreen(
         meals: filteredMeals,
