a6dd13c7a25fc0ec92ed640d985e85831f4757f4
KotenkoNata <kotenkonata8@gmail.com>
2024-12-09 16:48:12 +0100

    feat: Configuring Implicit Animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -29,9 +29,12 @@ class MealDetailsScreen extends ConsumerWidget {
             icon: AnimatedSwitcher(
                 duration: const Duration(microseconds: 30),
                     transitionBuilder: (child, animation) {
-                      return RotationTransition(turns: animation, child: child);
+                      return RotationTransition(
+                          turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                          child: child
+                      );
                     },
-                    child: Icon(isFavorite ? Icons.star : Icons.star_border)
+                    child: Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite))
             ),
           ),
         ],
