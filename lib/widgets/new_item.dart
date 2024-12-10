92283138509a4589b27e1d77f5573ca342da6b50
KotenkoNata <kotenkonata8@gmail.com>
2024-12-10 16:10:52 +0100

    feat: A Form-aware Dropdown Button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_dart_course/data/categories.dart';
 
 class NewItem extends StatefulWidget{
   const NewItem({super.key});
@@ -31,6 +31,39 @@ class _NewItemState extends State<NewItem>{
                     return 'Demo...';
                   },
                 ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Quantity'),
+                        ),
+                        initialValue: '1',
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: DropdownButtonFormField(items: [
+                        for(final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(category.value.title),
+                            ],
+                          ),
+                          ),
+                      ], onChanged: (value){}),
+                    ),
+                  ],
+                )
               ],
             )
         ),
