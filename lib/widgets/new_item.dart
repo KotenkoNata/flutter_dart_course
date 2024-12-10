2581986288ba54161c9912bcb4c02c4ad28d8ca8
KotenkoNata <kotenkonata8@gmail.com>
2024-12-10 16:27:21 +0100

    feat: Getting Form Access via a Global Key

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -11,6 +11,12 @@ class NewItem extends StatefulWidget{
 }
 
 class _NewItemState extends State<NewItem>{
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem (){
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,7 @@ class _NewItemState extends State<NewItem>{
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
             child: Column(
               children: [
                 TextFormField(
@@ -42,6 +49,7 @@ class _NewItemState extends State<NewItem>{
                         decoration: const InputDecoration(
                           label: Text('Quantity'),
                         ),
+                        keyboardType: TextInputType.number,
                         initialValue: '1',
                         validator: (value) {
                           if(value == null || value.isEmpty || int.tryParse(value) == null || int.tryParse(value)! <= 0){
@@ -77,8 +85,10 @@ class _NewItemState extends State<NewItem>{
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    TextButton(onPressed: (){}, child: const Text('Reset')),
-                    ElevatedButton(onPressed: (){}, child: const Text('Add Item'))
+                    TextButton(onPressed: (){
+                      _formKey.currentState!.reset();
+                    }, child: const Text('Reset')),
+                    ElevatedButton(onPressed: _saveItem, child: const Text('Add Item'))
                   ],
                 ),
               ],
