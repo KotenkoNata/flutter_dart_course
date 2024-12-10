021011a51a3e7f6773b8b5c5303657c1ae790532
KotenkoNata <kotenkonata8@gmail.com>
2024-12-10 16:38:20 +0100

    feat: Extracting Entered Values

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dart_course/data/categories.dart';
+import 'package:flutter_dart_course/models/categories.dart';
 
 class NewItem extends StatefulWidget{
   const NewItem({super.key});
@@ -12,9 +13,15 @@ class NewItem extends StatefulWidget{
 
 class _NewItemState extends State<NewItem>{
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem (){
-    _formKey.currentState!.validate();
+    if(_formKey.currentState!.validate()){
+      _formKey.currentState!.save();
+      print(_enteredName);
+    }
   }
 
   @override
@@ -40,6 +47,9 @@ class _NewItemState extends State<NewItem>{
                     }
                     return null;
                   },
+                  onSaved: (value){
+                    _enteredName = value!;
+                  },
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
@@ -50,18 +60,23 @@ class _NewItemState extends State<NewItem>{
                           label: Text('Quantity'),
                         ),
                         keyboardType: TextInputType.number,
-                        initialValue: '1',
+                        initialValue: _enteredQuantity.toString(),
                         validator: (value) {
                           if(value == null || value.isEmpty || int.tryParse(value) == null || int.tryParse(value)! <= 0){
                             return 'Must be a valid, positive number';
                           }
                           return null;
                         },
+                        onSaved: (value){
+                          _enteredQuantity = int.parse(value!);
+                        },
                       ),
                     ),
                     const SizedBox(width: 8),
                     Expanded(
-                      child: DropdownButtonFormField(items: [
+                      child: DropdownButtonFormField(
+                        value: _selectedCategory,
+                          items: [
                         for(final category in categories.entries)
                           DropdownMenuItem(
                             value: category.value,
@@ -77,7 +92,11 @@ class _NewItemState extends State<NewItem>{
                             ],
                           ),
                           ),
-                      ], onChanged: (value){}),
+                      ], onChanged: (value){
+                          setState(() {
+                            _selectedCategory = value!;
+                          });
+                      }),
                     ),
                   ],
                 ),
