cef33f22184e5157e2c88d3a09d68a21d476225b
KotenkoNata <kotenkonata8@gmail.com>
2024-12-12 16:53:24 +0100

    feat: Error Response Handling

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -17,6 +17,7 @@ class _GroceryListState extends State<GroceryList> {
 
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -28,6 +29,12 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https('flutter-app-a47f8-default-rtdb.firebaseio.com', 'shopping-list.json');
     final response = await http.get(url);
 
+    if(response.statusCode >= 400){
+      setState(() {
+        _error = 'Failed to catch data. Please try again later.';
+      });
+    }
+    
     final Map<String, dynamic>listData = json.decode(response.body);
 
     final List<GroceryItem>loadedItems = [];
@@ -97,6 +104,12 @@ class _GroceryListState extends State<GroceryList> {
             trailing: Text(_groceryItems[index].quantity.toString()),
           ),
         ),
+      );
+    }
+
+    if(_error != null){
+      content = Center(
+        child: Text(_error!),
       );
     }
 
