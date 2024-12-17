81981c7dc977060457445cffea5bf667e72dbacd
KotenkoNata <kotenkonata8@gmail.com>
2024-12-17 16:32:12 +0100

    feat: Adding Places with Provider & Displaying Places

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dart_course/providers/user_places.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddPlaceScreen extends StatefulWidget{
+class AddPlaceScreen extends ConsumerStatefulWidget{
   const AddPlaceScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
+  ConsumerState<AddPlaceScreen> createState() {
     return _AddPlaceScreenState();
   }
 }
 
-class _AddPlaceScreenState extends State<AddPlaceScreen>{
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen>{
   final _titleController = TextEditingController();
+
+  void _savePlace(){
+    final enteredTitle = _titleController.text;
+
+    if(enteredTitle.isEmpty){
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    Navigator.of(context).pop();
+  }
 
   @override
     void dispose() {
@@ -35,7 +48,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen>{
             ),
             const SizedBox(height: 16,),
             ElevatedButton.icon(
-              onPressed: (){},
+              onPressed: _savePlace,
               icon: const Icon(Icons.add),
               label: const Text('Add place'),
             )
