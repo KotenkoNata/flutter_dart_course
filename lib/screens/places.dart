4b43c3e9e82549ea3dd098f5a82ae0f08b89a6d1
KotenkoNata <kotenkonata8@gmail.com>
2024-12-17 16:13:25 +0100

    feat: Adding an "Add Place" Screen

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dart_course/screens/add_place.dart';
 import 'package:flutter_dart_course/widgets/places_list.dart';
 
 class PlacesScreen extends StatelessWidget{
@@ -11,7 +12,13 @@ class PlacesScreen extends StatelessWidget{
         title: const Text("Your Places"),
         actions: [
           IconButton(
-              onPressed: (){},
+              onPressed: (){
+                Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx)=>const AddPlaceScreen(),
+                    ),
+                );
+              },
               icon: const Icon(Icons.add),
           ),
         ],
