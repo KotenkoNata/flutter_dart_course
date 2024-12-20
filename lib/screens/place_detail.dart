b59c990ebb11f7eb101729e82ccb592e7136dc64
KotenkoNata <kotenkonata8@gmail.com>
2024-12-20 17:05:13 +0100

    feat: Displaying the Picked Place on a Dynamic Map

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dart_course/models/place.dart';
+import 'package:flutter_dart_course/screens/map.dart';
 
 class PlaceDetailScreen extends StatelessWidget{
   const PlaceDetailScreen({super.key, required this.place});
@@ -34,9 +35,18 @@ class PlaceDetailScreen extends StatelessWidget{
               right: 0,
               child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 70,
-                    backgroundImage: NetworkImage(locationImage),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (ctx) => MapScreen(location: place.location, isSelecting: false,),
+                          ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      radius: 70,
+                      backgroundImage: NetworkImage(locationImage),
+                    ),
                   ),
                   Container(
                     alignment: Alignment.center,
