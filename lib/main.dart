7de4163c21193342010d2baa2b53f5fba4afeeeb
KotenkoNata <kotenkonata8@gmail.com>
2024-12-24 16:47:43 +0100

    feat: App & Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dart_course/screens/places.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-final colorScheme = ColorScheme.fromSeed(
-  brightness: Brightness.dark,
-  seedColor: const Color.fromARGB(255, 102, 6, 247),
-  surface: const Color.fromARGB(255, 56, 49, 66),
-);
-
-final theme = ThemeData().copyWith(
-  scaffoldBackgroundColor: colorScheme.surface,
-  colorScheme: colorScheme,
-  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
-    titleSmall: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-    titleMedium: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-    titleLarge: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-);
-
-void main() async{
-  await dotenv.load(fileName: ".env");
-  runApp(
-    const ProviderScope(child: MyApp()),
-  );
+void main() {
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Great Places',
-      theme: theme,
-      home: const PlacesScreen(),
+        title: 'FlutterChat',
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 63, 17, 177)),
+        ),
+        home: ...
     );
   }
 }
