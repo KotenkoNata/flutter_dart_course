63148135f7393095d143b708e2f14906ed9228ca
KotenkoNata <kotenkonata8@gmail.com>
2024-12-24 16:29:53 +0100

    feat: Using a FutureBuilder for Loading Data

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,11 +4,26 @@ import 'package:flutter_dart_course/screens/add_place.dart';
 import 'package:flutter_dart_course/widgets/places_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends ConsumerWidget{
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
-  
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
@@ -16,23 +31,30 @@ class PlacesScreen extends ConsumerWidget{
         title: const Text("Your Places"),
         actions: [
           IconButton(
-              onPressed: (){
-                Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (ctx)=>const AddPlaceScreen(),
-                    ),
-                );
-              },
-              icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
           ),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder(future: future, builder: builder)PlacesList(
-            places: userPlaces,
-        ),
-      ),
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlacesList(
+                        places: userPlaces,
+                      ),
+          )),
     );
   }
 }
