b9568d2b482c2dc8827ed9988e44fa7c0d49faa4
KotenkoNata <kotenkonata8@gmail.com>
2025-01-17 17:00:48 +0100

    feat: Uploading Images To Firebase

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_dart_course/widgets/user_image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 final _firebase = FirebaseAuth.instance;
 
@@ -37,6 +38,12 @@ class _AuthScreenState extends State<AuthScreen>{
             email: _enteredEmail, password: _enteredPassword);
       }else{
         final userCredentials = await _firebase.createUserWithEmailAndPassword(email: _enteredEmail, password: _enteredPassword);
+
+        final storageRef = FirebaseStorage.instance.ref().child('user_images').child('${userCredentials.user!.uid}.jpg');
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+        print(imageUrl);
       }
     } on FirebaseAuthException catch(error){
         ScaffoldMessenger.of(context).clearSnackBars();
